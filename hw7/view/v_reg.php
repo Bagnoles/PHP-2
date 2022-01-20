0633bc65cd8f8699b2b6a230656c017404950656
Anna <anna.mironova89@yandex.ru>
2022-01-20 22:06:40 +0300

    start home work 7

---
+++ hw7/view/v_reg.php
@@ -0,0 +1,8 @@
+<form method="post">
+    <p><?=$text?></p>
+    <p>Имя</p><input type="text" name="name" required>
+    <p>Логин</p><input type="text" name="login" required>
+    <p>Пароль</p><input type="password" name="pass" required><br>
+    <input type="submit">
+
+</form>
